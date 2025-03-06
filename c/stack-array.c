33c6b317a8d7a1ed57c25293c76fedea8ab7d9f0
Mohammed-Hafeez-99 <upscsskzm805@gmail.com>
2025-03-06 20:19:31 +0530

    c files

---
+++ c/stack-array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// pop()
+// push()
+// display()
+// top()
+// exit()
+
+int main(){
+    int stack[];
+    printf("choose a valid option");
+    printf("\n1.push\n2.pop\n3.display stack\n4.top\n5.exit\n");
+}
+
+pop()[
+
+]
